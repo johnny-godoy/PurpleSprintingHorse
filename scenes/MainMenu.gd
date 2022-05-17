88c41a90726e8c032df8212d66c2bae02b1cdfbf
salocinc <nicolascalbucuravillalobos@gmail.com>
2022-05-17 18:33:24 -0400

    Menu.2
    Avance de menu.

--- scenes/MainMenu.gd
+++ scenes/MainMenu.gd
@@ -1,23 +1,17 @@
-extends MarginContainer
+extends Node2D
 
-onready var coloreamiento = $Panel/VBoxContainer/VBoxContainer/Coloreamiento
-onready var camino = $Panel/VBoxContainer/VBoxContainer/Camino
-onready var exit = $Panel/VBoxContainer/VBoxContainer/Exit
-# Called when the node enters the scene tree for the first time.
+onready var exit = $Exit
+onready var coloreamiento = $coloreamiento
+onready var camino = $shortestpath
+
 func _ready():
-	coloreamiento.connect("pressed",self, "_on_coloreamiento_pressed")
-	camino.connect("pressed",self, "_on_camino_pressed")
 	exit.connect("pressed",self, "_on_exit_pressed")
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-func _on_coloreamiento_pressed():
-	get_tree().change_scene("res://coloring/Level 1.tscn")
-
-func _on_camino_pressed():
-	get_tree().change_scene("res://scenes/shortest_path/hito_1/nivel_ejemplo_1.tscn")
+	coloreamiento.connect("pressed",self,"_on_coloreamiento_pressed")
+	camino.connect("pressed",self,"_on_camino_pressed")
 
 func _on_exit_pressed():
 	get_tree().quit()
+
+func _on_coloreamiento_pressed():
+	get_tree().change_scene("res://scenes/NivelesColoreamiento.tscn")
+
