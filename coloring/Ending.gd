7c57fdb2971ca5170b05f59270ecacc020a6eb39
ClavitoB <johnny.godoy@ing.uchile.cl>
2022-07-05 21:30:48 -0400

    crea tutoriales

---
+++ coloring/Ending.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+func _ready():
+	var instruction_overlay = $Horse_Overlay
+	var texto_bienvenida = 'Oh...hola.'
+
+	var script = ['Tengo malas noticias, me van a expulsar de la Academia Equina de Arte...Por falta de integridad académica',
+	'Las buenas noticias son que me conseguí un trabajo en construcción. ¡Deberías visitarme allá!',
+	'Hmmm, aunque acá quedó un último desafío...']
+	
+	yield(instruction_overlay.prompt_text(texto_bienvenida), "completed")
+	yield(instruction_overlay.prompt_iterables(script), "completed")
+	instruction_overlay.visible = false
---
+++ coloring/Tutorial 1.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+func _ready():
+	var instruction_overlay = $Horse_Overlay
+	var texto_bienvenida = '¡Hola!, yo soy el Caballo Púrpura.'
+
+	var script = ['Estudio en la importante Academia Equina de Arte....pero necesito tu ayuda con mi tarea',
+	'Debes pintar los círculos en la pantalla, ¡es fácil!.',
+	'En tu caballete tienes una selección de colores, ¡elige los que más que gusten!',
+	'Presiona un color para seleccionarlo, y luego presiona un círculo para pintarlo de ese color. Si quieres borrar un color, usa el blanco :)',
+	'Hay una regla muy importante que no puedes olvidar: Si dos círculos están unidos por una línea azul, ¡no los puedes pintar del mismo color!',
+	'Pero eso es suficiente charla, ¡vamos a divertirnos!']
+	
+	yield(instruction_overlay.prompt_text(texto_bienvenida), "completed")
+	yield(instruction_overlay.prompt_iterables(script), "completed")
+	instruction_overlay.visible = false
