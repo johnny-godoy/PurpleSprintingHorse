8caba63b78a457e82d55629a75c6420e0e985c6e
Agustin Gonzalez <agustin.gonzalez.u@gmail.com>
2022-07-20 02:44:01 -0400

    Estrellas en camino mas corto

--- scenes/NivelesCamino.gd
+++ scenes/NivelesCamino.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+onready var manager = ShortestPathManager
 onready var atras = $atras
 onready var seleccion = $Scroll/Seleccion
 
@@ -10,12 +11,24 @@ func _ready():
 	var seleccionables = seleccion.get_children()
 	for nivel in seleccionables.size():
 		var seleccionable = seleccionables[nivel]
+		seleccionable.shortest_path()
 		seleccionable.index.text = str(nivel + 1)
 		# seleccionable.completion.text = Según lo que carguen del guardado
 		seleccionable.texture_normal = load("res://assets/shortest_path/lvl%d.png" % (nivel + 1))
 		seleccionable.scene = "res://scenes/shortest_path/niveles/nivel_%d.tscn" % (nivel + 1)
-
+		seleccionable.star_texture(_star_texture(nivel + 1))
 
 func _on_atras_pressed():
 	# warning-ignore:return_value_discarded
 	get_tree().change_scene("res://scenes/MainMenu.tscn")
+
+func _star_texture(lvl):
+	var num_stars = manager.load_stars(lvl)
+	if num_stars == 3:
+		return load("res://assets/shortest_path/3_estrellas.PNG")
+	elif num_stars == 2:
+		return load("res://assets/shortest_path/2_estrellas.PNG")
+	elif num_stars == 1:
+		return load("res://assets/shortest_path/1_estrellas.PNG")
+	else:
+		return load("res://assets/shortest_path/0_estrellas.PNG")
