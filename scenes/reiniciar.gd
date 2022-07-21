881f8510caacb35c81382807564902d014a88406
ClavitoB <johnny.godoy@ing.uchile.cl>
2022-07-21 18:39:53 -0400

    Update reiniciar.gd
    ahora reinicia a coloramiento tmbn

--- scenes/reiniciar.gd
+++ scenes/reiniciar.gd
@@ -3,6 +3,9 @@ extends TextureButton
 onready var audio = $AudioStreamPlayer
 onready var manager = ShortestPathManager
 
+var coloring_file = File.new()
+var reset_value = str([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+
 export(String, FILE) var stream_file = "res://assets/sonido/menuhit.wav"
 export(String, FILE) var next_scene = ""
 
@@ -15,5 +18,10 @@ func _ready() -> void:
 func on_button_pressed() -> void:
 	audio.play()
 	yield(audio, "finished")
+
 	manager.restart_levels()
+	coloring_file.open("res://coloring/base/coloring_progress.dat", File.WRITE)
+	coloring_file.store_string(reset_value)
+	coloring_file.close()
+
 	texture_normal = load("res://assets/IconPack/IconPackByAndelRodis/pngonebyone/trashcanopen.png")
